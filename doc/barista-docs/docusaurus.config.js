968ecdcde4035e317b1b886ed7bfe2ca1652d537
PKNelson <pknelson@optum.com>
2020-01-29 14:34:56 -0600

    initializing docusaurus
    just initializing to validate configuration

---
+++ doc/barista-docs/docusaurus.config.js
@@ -0,0 +1,91 @@
+module.exports = {
+  title: 'Barista',
+  tagline: 'open source license and vulnerability management',
+  url: 'https://optum.github.io/barista',
+  baseUrl: '/barista/',
+  favicon: 'img/barista-icon.ico',
+  organizationName: 'Optum', // Usually your GitHub org/user name.
+  projectName: 'barista', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Barista Docs',
+      logo: {
+        alt: 'Barista logo',
+        src: 'img/barista.png',
+      },
+      links: [
+        {to: 'docs/doc1', label: 'Docs', position: 'left'},
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/doc1',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Optum, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};

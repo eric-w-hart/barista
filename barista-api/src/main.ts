813afd00d00d20f493eb6791cbcde60b4cac4d51
Ben Woodring <benajmain.woodring@optum.com>
2020-03-02 10:12:05 -0600

    Update for cors policy

--- barista-api/src/main.ts
+++ barista-api/src/main.ts
@@ -85,6 +85,12 @@ async function bootstrap() {
 
   app.use(arena);
   app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
 
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.API_PORT || 3000);

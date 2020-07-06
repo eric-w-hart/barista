83bafe2e70c8146b3374085ff541ba0c07b5822e
Carson DeSotel <carson.desotel@gmail.com>
2020-07-06 13:09:44 -0500

    generates basic charts on the home page

--- barista-web/src/app/features/home/home.component.ts
+++ barista-web/src/app/features/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PieChartComponent } from '@app/shared/app-components/charts/pie-chart/pie-chart.component';
+import { BarChartComponent } from '@app/shared/app-components/charts/bar-chart/bar-chart.component';
+
+import { data } from './temporary_display_data';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  constructor(){ }
 
-  constructor() { }
+  // create a variable for each dataset we want stored on the page.
+  // for consistency, variables end in Data, async variables end in Data$
+  topComponentLicenseData = data; 
+  // TODO: create DTO for each dataset, wrap it in observable
 
+  /**
+   * All initialization of the datasets & graphs occurs here. 
+   * Handles subscribing of data async's into data vars. 
+   */
   ngOnInit(): void {
+
   }
 
 }

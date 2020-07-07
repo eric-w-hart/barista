d1a84e529dc21c5e4014e520e2f31ddb39bdd262
Carson DeSotel <carson.desotel@gmail.com>
2020-07-07 09:24:32 -0500

    creates front page with flexbox

--- barista-web/src/app/features/home/temporary_display_data.ts
+++ barista-web/src/app/features/home/temporary_display_data.ts
@@ -1,58 +1,18 @@
 export var data = [
     {
-      "name": "Germany",
-      "value": 8940000
+      "name": "Apache License",
+      "value": 1000
     },
     {
-      "name": "USA",
-      "value": 5000000
+      "name": "MIT License",
+      "value": 250
     },
     {
-      "name": "France",
-      "value": 7200000
-    }
-  ];
-  
-  export var multi = [
-    {
-      "name": "Germany",
-      "series": [
-        {
-          "name": "2010",
-          "value": 7300000
-        },
-        {
-          "name": "2011",
-          "value": 8940000
-        }
-      ]
+      "name": "Common Development and Distribution License",
+      "value": 36
     },
-  
     {
-      "name": "USA",
-      "series": [
-        {
-          "name": "2010",
-          "value": 7870000
-        },
-        {
-          "name": "2011",
-          "value": 8270000
-        }
-      ]
-    },
-  
-    {
-      "name": "France",
-      "series": [
-        {
-          "name": "2010",
-          "value": 5000002
-        },
-        {
-          "name": "2011",
-          "value": 5800000
-        }
-      ]
+      "name": "Public Domain",
+      "value": 12
     }
   ];

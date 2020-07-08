d9532184ac4344e6e43519ab7d34ef9b6726fe93
Kevin-Bradt <67155765+Kevin-Bradt@users.noreply.github.com>
2020-07-08 15:10:08 -0500

    Adding horizontal bar charts for testing

---
+++ barista-web/src/app/shared/app-components/charts/horizontal-bar-chart/horizontal-bar-chart.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ChartBaseComponent } from '@app/shared/app-components/charts/chart-base/chart-base.component';
+
+@Component({
+  selector: 'app-horizontal-bar-chart',
+  templateUrl: './horizontal-bar-chart.component.html',
+  styleUrls: ['./horizontal-bar-chart.component.scss']
+})
+export class HorizontalBarChartComponent extends ChartBaseComponent implements OnInit {
+
+  constructor() { super(); };
+
+  showXAxis = true;
+  showYAxis = true;
+  showXAxisLabel = true;
+  showYAxisLabel = true;
+
+  @Input() xAxisLabel;
+  @Input() yAxisLabel;
+
+  ngOnInit(): void {
+  }
+
+}

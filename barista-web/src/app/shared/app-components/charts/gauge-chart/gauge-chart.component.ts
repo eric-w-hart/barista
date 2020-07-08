c2fd3db669f82b9b6dd9c39ccdf693efaba7e48d
Ryan Harvey <ryanhmn@gmail.com>
2020-07-08 15:16:40 -0500

    Adds gauge charts

---
+++ barista-web/src/app/shared/app-components/charts/gauge-chart/gauge-chart.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartBaseComponent } from '@app/shared/app-components/charts/chart-base/chart-base.component';
+
+@Component({
+  selector: 'app-gauge-chart',
+  templateUrl: './gauge-chart.component.html',
+  styleUrls: ['../chart-base/chart-base.component.scss',
+              './gauge-chart.component.scss']
+})
+export class GaugeChartComponent extends ChartBaseComponent implements OnInit {
+
+  constructor(){
+    super();
+  };
+
+  //Options
+  legendPosition: string = 'below';
+
+
+  ngOnInit(): void {
+  }
+
+}

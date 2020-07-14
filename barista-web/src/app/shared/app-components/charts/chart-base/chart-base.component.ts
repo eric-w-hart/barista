ba1327ce26e71f03b219b2ad1f5982b6d72877e1
Ryan Harvey <ryanhmn@gmail.com>
2020-07-14 15:27:55 -0500

    Adds custom colors to pie charts and allows color scheme to be an input

--- barista-web/src/app/shared/app-components/charts/chart-base/chart-base.component.ts
+++ barista-web/src/app/shared/app-components/charts/chart-base/chart-base.component.ts
@@ -6,12 +6,33 @@ export abstract class ChartBaseComponent implements OnInit {
   @Input() data: any[];
   @Input() legend: boolean;
   @Input() legendTitle: string;
+  @Input() colorScheme: any = { domain: ["#DA8156","#43AA8B","#FFBF66","#08C5D1", "#B44346", "#2C5860"]};
+
 
   gradient = false;
 
-  colorScheme = {
-    domain: ["#DA8156","#43AA8B","#FFBF66","#08C5D1", "#B44346", "#2C5860"]
-   };
+  customColors = [
+   {
+     name: "CRITICAL",
+     value: '#dd3333'
+   },
+   {
+     name: "HIGH",
+     value: '#ff8333'
+   },
+   {
+     name: "MEDIUM",
+     value: '#eeee33'
+   },
+   {
+     name: "LOW",
+     value: '#338833'
+   },
+   {
+     name: "UNKNOWN",
+     value: '#aaaaaa'
+   }
+ ];
 
   ngOnInit() {}
 

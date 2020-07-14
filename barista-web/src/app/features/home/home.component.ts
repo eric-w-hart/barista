94d557d2141e22a4a3de9a4a11f073b8f46af7ec
jorden-lee <67109239+jorden-lee@users.noreply.github.com>
2020-07-14 12:43:10 -0500

    Created a line graph to use later and cleaned up
    created a line-graph like gauge graph, bar graph, and pie graph to be used later. Cleaned up old hardcoded data used for display purposes

--- barista-web/src/app/features/home/home.component.ts
+++ barista-web/src/app/features/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { data } from './temporary_display_data';
 import { StatsApiService } from '@app/shared/api/api/stats-api.service';
 import { observable } from 'rxjs';
 

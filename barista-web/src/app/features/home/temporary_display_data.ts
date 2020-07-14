631f4d8bc382cd029b86053e591f4a41e6783cad
jorden-lee <67109239+jorden-lee@users.noreply.github.com>
2020-07-14 14:59:09 -0500

    dummy data and line chart

--- barista-web/src/app/features/home/temporary_display_data.ts
+++ barista-web/src/app/features/home/temporary_display_data.ts
@@ -1,18 +1,68 @@
 export var data = [
-    {
-      "name": "Apache License",
-      "value": 1000
-    },
-    {
-      "name": "MIT License",
-      "value": 250
-    },
-    {
-      "name": "Common Development and Distribution License",
-      "value": 36
-    },
-    {
-      "name": "Public Domain",
-      "value": 12
-    }
-  ];
+  {
+    "name": "Germany",
+    "series": [
+      {
+        "name": "1990",
+        "value": 62000000
+      },
+      {
+        "name": "2010",
+        "value": 73000000
+      },
+      {
+        "name": "2011",
+        "value": 89400000
+      }
+    ]
+  },
+
+  {
+    "name": "USA",
+    "series": [
+      {
+        "name": "1990",
+        "value": 250000000
+      },
+      {
+        "name": "2010",
+        "value": 309000000
+      },
+      {
+        "name": "2011",
+        "value": 311000000
+      }
+    ]
+  },
+
+  {
+    "name": "France",
+    "series": [
+      {
+        "name": "1990",
+        "value": 58000000
+      },
+      {
+        "name": "2010",
+        "value": 50000020
+      },
+      {
+        "name": "2011",
+        "value": 58000000
+      }
+    ]
+  },
+  {
+    "name": "UK",
+    "series": [
+      {
+        "name": "1990",
+        "value": 57000000
+      },
+      {
+        "name": "2010",
+        "value": 62000000
+      }
+    ]
+  }
+];

ecef39d3650ac5697bf5f28ea2d5ea49467dc0bd
jorden-lee <67109239+jorden-lee@users.noreply.github.com>
2020-07-20 10:11:40 -0500

    Used persistent header
    Used persistent header instead of page header

--- barista-web/src/app/shared/layout/header/header.component.ts
+++ barista-web/src/app/shared/layout/header/header.component.ts
@@ -55,8 +55,17 @@ export class HeaderComponent implements OnInit, OnDestroy{
   }
   async homeLink(){
     await this.router.navigate(['/home']);
+    window.location.reload();
   }
   profileBtn(){
     this.isVisible = true;
+  }
+  isSignIn(){
+    if(this.router.url.endsWith('/signin')){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }

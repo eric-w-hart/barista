b6699e0ab9657a751a9a3b897a951a74752abb27
Ben Woodring <benajmain.woodring@optum.com>
2020-07-24 10:15:09 -0500

    Update chart-base.component.ts

--- barista-web/src/app/shared/app-components/charts/chart-base/chart-base.component.ts
+++ barista-web/src/app/shared/app-components/charts/chart-base/chart-base.component.ts
@@ -6,11 +6,10 @@ export abstract class ChartBaseComponent implements OnInit {
   @Input() data: any[];
   @Input() legend: boolean;
   @Input() legendTitle: string;
-  // @Input() colorScheme: any = { domain: ["#DA8156","#43AA8B","#FFBF66","#08C5D1", "#B44346", "#2C5860"]};
-  @Input() colorScheme: any = { domain: ['#7c6146', '#4c3b26', '#9f956b', '#595433', '#b4ac9c', '#B4C6D4', '#746F6E'] };
+  @Input() colorScheme: any = { domain: ['#DA8156', '#43AA8B', '#FFBF66', '#08C5D1', '#B44346', '#2C5860'] };
 
   gradient = false;
-
+  coffeeColors: any = { domain: ['#7c6146', '#4c3b26', '#9f956b', '#595433', '#b4ac9c', '#B4C6D4', '#746F6E'] };
   customColors = [
     {
       name: 'CRITICAL',

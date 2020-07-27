6bd14541aa3afefbed9e53420d4af53e9bee557b
Carson DeSotel <carson.desotel@gmail.com>
2020-07-27 08:58:06 -0500

    moves all banner code to a new banner component
    adds:
    * banner.component.ts
    * banner.component.spec.ts
    * banner.component.scss
    
    The HTML is contained in banner.component.ts

---
+++ barista-web/src/app/features/home/banner/banner.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { UserApiService } from '@app/shared/api/api/user-api.service';
+import { UserInfo } from '@app/shared/api/model/user-info';
+
+@Component({
+  selector: 'app-banner',
+  template: `
+  <!-- Banner -->
+  <div class="banner">
+    <div *ngIf="!isLoggedIn">
+      <div class="image">
+        <img src="assets/images/barista_logo_text_removed.png">
+      </div>
+      <div class="mat-headline banner_text" style="text-align: center;">
+        Open Source Governance <br> Ensuring You Always Have a Great Brew
+      </div>
+      <div class="button">
+        <a mat-raised-button [href]="'https://optum.github.io/barista/'" target="_blank">
+          <span>
+            Learn More
+          </span>
+        </a>
+      </div>
+    </div>
+    <div class="row button-toggle" *ngIf="isLoggedIn">
+      <mat-button-toggle-group value="Organization">
+        <div class="button">
+          <mat-button-toggle (click)="changeDataset()" value="User" style="margin-right: 5px;"><span>User</span></mat-button-toggle>
+        </div>
+        <div class="button"> 
+          <mat-button-toggle (click)="changeDataset('%')" value="Organization" style="margin-left: 5px;"><span>Organization</span></mat-button-toggle>
+        </div>
+      </mat-button-toggle-group>
+    </div>
+  </div>
+  <!-- End of Banner -->
+  `,
+  styles: ['../home.component.scss',],
+})
+export class BannerComponent implements OnInit {
+  // Member Variables:
+  @Input() isLoggedIn: boolean;
+  dataset: string;
+  @Output() changeDatasetEvent = new EventEmitter<string>();
+  user: UserInfo;
+
+  constructor(private userApi: UserApiService) { this.dataset = ''; }
+  ngOnInit(): void { 
+    // pass through userID
+    this.userApi.userMeGet().subscribe((response) => {
+      this.user = response;
+    })
+  }
+
+  sendDataset(dataset?: string){
+    this.changeDatasetEvent.emit(this.dataset);
+  }
+
+  changeDataset(dataset?: string){
+    if(dataset){
+      // pass through generic selector
+      this.dataset = dataset;
+    } else {
+      this.dataset = this.user.id;
+    }
+
+    this.sendDataset(this.dataset);
+  }
+
+}

705a1cdf1a42949a36c11191313b33d7012e1ca5
Carson DeSotel <carson.desotel@gmail.com>
2020-07-27 09:18:43 -0500

    adds vertical bar chart component for home

---
+++ barista-web/src/app/features/home/chart-bar-vertical/chart-bar-vertical.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-bar-vertical',
+  template: `
+  <mat-card>
+  <mat-card-title>
+    <div>
+      {{ title }}
+    <span style="float:right;">
+        <a class="info">
+          <img src="assets/images/help-circle-outline.svg" />
+          <span>
+            <mat-card>
+              <mat-card-content>
+                {{ description }}
+              </mat-card-content>
+            </mat-card>
+          </span>
+        </a>
+      </span>
+    </div>
+  </mat-card-title>
+  <mat-card-content>
+    <div *ngIf="!dataset || dataset.length < 1">
+      <h2> No data. </h2>
+    </div>
+    <app-bar-chart *ngIf="dataset" 
+      [data]="dataset" 
+      [legend]="true"
+      [legendTitle]="'Months:'" 
+      [xAxisLabel]="'Month'" 
+      [yAxisLabel]="'Count'">
+    </app-bar-chart>
+  </mat-card-content>
+</mat-card>
+  `,
+  styles: []
+})
+export class ChartBarVerticalComponent implements OnInit {
+  // Member Variables:
+  @Input() dataset: any;
+  @Input() title: string;
+  @Input() description: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

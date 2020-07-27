fc413d8bc4dbe6562e66de6cb97e96deeb3fd84a
jorden-lee <67109239+jorden-lee@users.noreply.github.com>
2020-07-27 13:18:05 -0500

    Update home.component.ts
    just testing

--- barista-web/src/app/features/home/home.component.ts
+++ barista-web/src/app/features/home/home.component.ts
@@ -51,7 +51,6 @@ export class HomeComponent implements OnInit, OnChanges {
     this.isLoading = true;
     this.dataset = '%';
     this.getDatasets();
-    this.isLoading = false;
   }
 
   ngOnChanges():void {
@@ -72,7 +71,6 @@ export class HomeComponent implements OnInit, OnChanges {
     }
 
     this.ngOnChanges();
-    this.isLoading = false;
   }
 
   getDatasets(){
@@ -85,30 +83,36 @@ export class HomeComponent implements OnInit, OnChanges {
 
     this.statsApi.statsVulnerabilitiesGet(this.dataset).subscribe((response) => {
       this.topVulnerabilities = response;
+      this.isLoading = false;
     });
 
     this.statsApi.statsComponentsScansGet(this.dataset).subscribe((response) => {
       this.topComponentScansData = response;
+      this.isLoading = false;
     })
 
     this.statsApi.statsProjectsGet(this.dataset).subscribe((response) => {
       response = this.parseMonth(response);
       this.projectsAddedMonthly = response;
+      this.isLoading = false;
     })
 
     this.statsApi.statsProjectsScansGet(this.dataset).subscribe((response) => {
       response = this.parseMonth(response);
       this.monthlyProjectScans = response;
+      this.isLoading = false;
     })
 
     this.statsApi.statsHighVulnerabilityGet(this.dataset).subscribe((response) => {
       var displayName = this.displaySeverity(response, this.vulnerabilityThreshold);
       this.highVulnerability = [{"name": displayName, "value": response}];
+      this.isLoading = false;
     })
 
     this.statsApi.statsLicenseOnComplianceGet(this.dataset).subscribe((response) => {
       var displayName = this.displaySeverity(response, this.licenseThreshold);
       this.licenseOnCompliance = [{"name": displayName, "value": response}];
+      this.isLoading = false;
     })
   }
 

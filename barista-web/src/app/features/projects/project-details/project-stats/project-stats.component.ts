63cf7d232ba86addcb4285fc65b04c6780d2c2c7
jorden-lee <67109239+jorden-lee@users.noreply.github.com>
2020-07-30 15:44:03 -0500

    added all possible critical vulnerabilites
    Added all of the 0 value critical vulnerabilities

--- barista-web/src/app/features/projects/project-details/project-stats/project-stats.component.ts
+++ barista-web/src/app/features/projects/project-details/project-stats/project-stats.component.ts
@@ -8,6 +8,8 @@ import { ChartElementDto } from '@app/shared/api/model/chart-element-dto';
 import * as _ from 'lodash';
 import { Observable } from 'rxjs';
 import { first, map } from 'rxjs/operators';
+import { concat } from 'lodash';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-project-stats',
@@ -25,6 +27,8 @@ export class ProjectStatsComponent implements OnInit {
 
   vulnerabilityData;
   @Input() vulnerabilityData$: Observable<ProjectDistinctVulnerabilityDto>;
+
+  severityArray: ChartElementDto[] = [ {'name': "LOW", 'value': 0},  {'name': "MODERATE", 'value': 0},  {'name': "MEDIUM", 'value': 0},  {'name': "HIGH", 'value': 0},  {'name': "CRITICAL", 'value': 0}];
 
   ngOnInit() {
     if (this.licenseData$) {
@@ -72,10 +76,18 @@ export class ProjectStatsComponent implements OnInit {
         .pipe(
           first(),
           map(items => {
-            const data: ChartElementDto[] = _.map(items, (item: any) => {
-              return {'name': item.severity, 'value': Number(item.count)};
+            var data: ChartElementDto[] = _.map(items, (item: any) => {
+              return {'name': item.severity + ": " + item.count, 'value': Number(item.count)};
             });
-            return data;
+            let severityNames: string[] = this.severityArray.map( (item) => item.name.toLowerCase() );
+            let dataNames: string[] = data.map((item) => item.name.toLowerCase());
+            let resultArray: string[] = severityNames.filter(item => dataNames.indexOf(item) < 0);
+            console.log(resultArray);
+            let returnArray: ChartElementDto[] = resultArray.map((item) => {return {'name': item.toUpperCase(), 'value': 0}}
+            );
+            console.log(returnArray);
+            console.log(data);
+            return data.concat(returnArray);
           }),
         )
         .subscribe(data => {

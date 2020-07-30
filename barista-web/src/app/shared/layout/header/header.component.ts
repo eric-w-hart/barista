e618f4678d53d313e76d5c0d6d40b6a43d552032
Ryan Harvey <ryanhmn@gmail.com>
2020-07-30 11:14:16 -0500

    Updates menu highlighting to focus on open menu

--- barista-web/src/app/shared/layout/header/header.component.ts
+++ barista-web/src/app/shared/layout/header/header.component.ts
@@ -58,6 +58,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   onHome: boolean = false;
   onProj: boolean = false;
   onAdmin: boolean = false;
+  menuClicked: boolean = false;
   role: string;
   @Output() public sidenavToggle = new EventEmitter();
   userName: string;
@@ -103,5 +104,17 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   isSignIn() {
     return this.router.url.endsWith('/signin');
+  }
+
+  menuOpened() {
+    this.menuClicked = true;
+  }
+
+  menuClosed() {
+    this.menuClicked = false;
+  }
+
+  receiveHelpEvent($event){
+    $event ? this.menuOpened() : this.menuClosed();
   }
 }

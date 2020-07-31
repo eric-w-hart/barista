b221afefad948ff858f2b7a9d981f74f04658cba
jorden-lee <67109239+jorden-lee@users.noreply.github.com>
2020-07-31 10:37:39 -0500

    fixed where legend was showing up when there wasn't data

--- barista-web/src/app/features/projects/project-details/project-stats/project-stats.component.ts
+++ barista-web/src/app/features/projects/project-details/project-stats/project-stats.component.ts
@@ -8,7 +8,7 @@ import { ChartElementDto } from '@app/shared/api/model/chart-element-dto';
 import * as _ from 'lodash';
 import { Observable } from 'rxjs';
 import { first, map } from 'rxjs/operators';
-import { concat } from 'lodash';
+import { concat, result } from 'lodash';
 import { element } from 'protractor';
 
 @Component({
@@ -78,15 +78,17 @@ export class ProjectStatsComponent implements OnInit {
             var data: ChartElementDto[] = _.map(items, (item: any) => {
               return {'name': item.severity, 'value': Number(item.count)};
             });
-            let dataNames: string[] = data.map((item) => item.name.toUpperCase());
-            let result: string[] = severityLabels.filter(item => dataNames.indexOf(item) < 0);
-            return data
-            .concat(result.map((item) => {
-              return {'name': item.toUpperCase(), 'value': 0}
-            }))
-            .sort((a, b) => {
-              return -(severityLabels.indexOf(a.name) - severityLabels.indexOf(b.name))
-            });
+            if(data.length !== 0){
+              let dataNames: string[] = data.map((item) => item.name.toUpperCase());
+              let result: string[] = severityLabels.filter(item => dataNames.indexOf(item) < 0);
+                return data 
+              .concat(result.map((item) => {
+                return {'name': item.toUpperCase(), 'value': 0}
+              }))
+              .sort((a, b) => {
+                return -(severityLabels.indexOf(a.name) - severityLabels.indexOf(b.name))
+              });
+            }
           }),
         )
         .subscribe(data => {

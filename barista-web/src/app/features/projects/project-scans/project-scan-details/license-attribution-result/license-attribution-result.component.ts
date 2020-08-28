131041fa4e9e17a5c955928c726b9799e3bf19a5
Ben Woodring <benajmain.woodring@optum.com>
2020-08-28 12:15:09 -0500

    updates for attribution text display

--- barista-web/src/app/features/projects/project-scans/project-scan-details/license-attribution-result/license-attribution-result.component.ts
+++ barista-web/src/app/features/projects/project-scans/project-scan-details/license-attribution-result/license-attribution-result.component.ts
@@ -1,4 +1,8 @@
+import { ProjectDistinctLicenseAttributionDto } from './../../../../../shared/api/model/project-distinct-license-attribution-dto';
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ScanApiService } from '@app/shared/api';
 
 @Component({
   selector: 'app-license-attribution-result',
@@ -6,8 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./license-attribution-result.component.scss'],
 })
 export class LicenseAttributionResultComponent implements OnInit {
-  constructor() {}
-  attribution = 'sdfgsdfgsfgß';
+  constructor(private router: Router, private route: ActivatedRoute, private scanApiService: ScanApiService) {}
+  attribution = '';
+  isLoadingAttribution = true;
 
-  ngOnInit() {}
+  ngOnInit() {
+    const scanId = this.route.snapshot.paramMap.get('scanId');
+    this.scanApiService.scanIdAttributionGet(Number(scanId)).subscribe((response) => {
+      response.forEach((element) => {
+        this.attribution += 'Package: ';
+        this.attribution += element.packageName + '\n\n';
+        this.attribution += 'License: ';
+        this.attribution += element.clearDefined?.license ? element.clearDefined.license : element.license;
+        this.attribution += '\n\n';
+        this.attribution += 'Copyrights: \n';
+        this.attribution += element.clearDefined?.copyrights ? element.clearDefined.copyrights : '';
+        this.attribution += '\n\n';
+        this.attribution += 'License Text: \n';
+        this.attribution += element.clearDefined?.licensetext ? element.clearDefined.licensetext : element.licenselink;
+        this.attribution += '\n\n';
+      });
+      this.isLoadingAttribution = false;
+    });
+  }
 }

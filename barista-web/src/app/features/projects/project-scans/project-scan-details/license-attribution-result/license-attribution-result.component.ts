f5092848ebdddcd5c0129904f916fd3b1f892ea2
Ben Woodring <benajmain.woodring@optum.com>
2020-09-01 15:44:25 -0500

    Update license-attribution-result.component.ts

--- barista-web/src/app/features/projects/project-scans/project-scan-details/license-attribution-result/license-attribution-result.component.ts
+++ barista-web/src/app/features/projects/project-scans/project-scan-details/license-attribution-result/license-attribution-result.component.ts
@@ -3,6 +3,7 @@ import { ProjectDistinctLicenseAttributionDto } from '@app/shared/api/model/proj
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AttributionService, LicenseScanResultItemApiService } from '@app/shared/api';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-license-attribution-result',
@@ -40,28 +41,31 @@ export class LicenseAttributionResultComponent implements OnInit {
         this.totalCount = response.length;
         console.log('total = ' + response.length);
         response.forEach((element) => {
-          this.attributionApiService.attributionByScanResultIdIdGet(Number(element.id)).subscribe((attribution) => {
-            this.attribution += 'Package: ';
-            this.attribution += attribution.packageName + '\n\n';
-            this.attribution += 'License: ';
-            this.attribution += attribution.clearDefined?.license
-              ? attribution.clearDefined.license
-              : attribution.license;
-            this.attribution += '\n\n';
-            this.attribution += 'Copyrights: \n';
-            this.attribution += attribution.clearDefined?.copyrights ? attribution.clearDefined.copyrights : '';
-            this.attribution += '\n\n';
-            this.attribution += 'License Text: \n';
-            this.attribution += attribution.clearDefined?.licensetext
-              ? attribution.clearDefined.licensetext
-              : attribution.licenselink;
-            this.attribution += '\n\n';
-            this.counter++;
-            console.log('counter = ' + this.counter);
-            if (this.totalCount === this.counter) {
-              this.isLoadingAttribution = false;
-            }
-          });
+          this.attributionApiService
+            .attributionByScanResultIdIdGet(Number(element.id))
+            .pipe(delay(50))
+            .subscribe((attribution) => {
+              this.attribution += 'Package: ';
+              this.attribution += attribution.packageName + '\n\n';
+              this.attribution += 'License: ';
+              this.attribution += attribution.clearDefined?.license
+                ? attribution.clearDefined.license
+                : attribution.license;
+              this.attribution += '\n\n';
+              this.attribution += 'Copyrights: \n';
+              this.attribution += attribution.clearDefined?.copyrights ? attribution.clearDefined.copyrights : '';
+              this.attribution += '\n\n';
+              this.attribution += 'License Text: \n';
+              this.attribution += attribution.clearDefined?.licensetext
+                ? attribution.clearDefined.licensetext
+                : attribution.licenselink;
+              this.attribution += '\n\n';
+              this.counter++;
+              console.log('counter = ' + this.counter);
+              if (this.totalCount === this.counter) {
+                this.isLoadingAttribution = false;
+              }
+            });
         });
       });
   }

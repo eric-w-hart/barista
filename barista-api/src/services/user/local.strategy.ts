cbccefe2105f2ac0b80a7739fa56a2405a1733e0
Woodring, Benjamin <benjamin.woodring@optum.com>
2022-01-21 13:17:35 -0600

    Update local.strategy.ts

--- barista-api/src/services/user/local.strategy.ts
+++ barista-api/src/services/user/local.strategy.ts
@@ -1,6 +1,6 @@
 import { LdapService } from '@app/services/user/ldap.service';
 import { UserService } from '@app/services/user/user.service';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 
@@ -9,7 +9,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly userService: UserService, private readonly ldapService: LdapService) {
     super();
   }
-
+  logger = new Logger('LdapService');
   async validate(username: string, password: string): Promise<any> {
     let user: any = null;
 
@@ -24,15 +24,23 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
         groups: ['group1', 'group1', 'group3'],
       };
     } else if (process.env.AUTH_TYPE === 'ldap') {
+      this.logger.log(`Ben - Before first validateuser`);
       user = await this.ldapService.validateUser(username, password, false);
+      this.logger.log(`Ben - After first validateuser`);
       if (!user) {
+        this.logger.log(`Ben - Before 2 validateuser`);
         user = await this.ldapService.validateUser(username, password, true);
+        this.logger.log(`Ben - After f2 validateuser`);
       }
       if (user) {
+        this.logger.log(`Ben - Before getgroups`);
         user.groups = await this.ldapService.getUserGroups(username, password);
+        this.logger.log(`Ben - After getgroups`);
       }
     } else {
+      this.logger.log(`Ben - Before 3 validateuser`);
       user = await this.userService.validateUser(username, password);
+      this.logger.log(`Ben - After 3 validateuser`);
     }
     if (!user) {
       throw new UnauthorizedException();

5eeb0e2138e255d651f987631b07db13a31e3967
Brian McCord <brian.mccord@optum.com>
2022-01-24 15:17:06 -0600

    Adding logging

--- barista-api/src/services/user/local.strategy.ts
+++ barista-api/src/services/user/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 
 import { LdapService } from '@app/services/user/ldap.service';
 import { PassportStrategy } from '@nestjs/passport';
@@ -10,6 +10,8 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly userService: UserService, private readonly ldapService: LdapService) {
     super();
   }
+
+  logger = new Logger('LdapService');
 
   async validate(username: string, password: string): Promise<any> {
     let user: any = null;
@@ -25,9 +27,13 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
         groups: ['group1', 'group1', 'group3'],
       };
     } else if (process.env.AUTH_TYPE === 'ldap') {
+      this.logger.log("Before the first validateUser");
       user = await this.ldapService.validateUser(username, password, false);
+      this.logger.log("After the first validateUser");
       if (!user) {
+        this.logger.log("Before the second validateUser");
         user = await this.ldapService.validateUser(username, password, true);
+        this.logger.log("After the second validateUser");
       }
       // if (user) {
       //   user.groups = await this.ldapService.getUserGroups(username, password);
